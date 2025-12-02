79b1dd0363e5ff3467d1999048f8e27f3489c0bc
miaoyuxun <amiemiaomiao@126.com>
2025-12-02 12:20:54 +0800

    fix

---
+++ chrome_setup.sh
@@ -0,0 +1,15 @@
+curl -O https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json
+
+grep -o '"version":"[^"]*"' last-known-good-versions.json | head -1 | cut -d'"' -f4 > chrome_version.txt
+
+stableVersion=$(cat chrome_version.txt)
+
+curl -O "https://storage.googleapis.com/chrome-for-testing-public/$stableVersion/linux64/chrome-linux64.zip"
+unzip chrome-win64.zip
+
+curl -O "https://storage.googleapis.com/chrome-for-testing-public/$stableVersion/linux64/chromedriver-linux64.zip"
+unzip chromedriver-win64.zip
+
+mkdir chromedriver
+
+cp chromedriver-linux64/chromedriver chromedriver/
